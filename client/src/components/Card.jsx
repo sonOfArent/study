b30014b5202527e88c0d5106fe1924df31655497
sonOfArent <humbleogier@gmail.com>
2022-12-04 19:30:01 -0600

    start

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -4,7 +4,6 @@ import './components.css'
 const Card = ({ cardData: { text } }) => {
   return (
     <div className='Card'>
-        {/* <h2 className='title'>{title ? title : "Sample Title"}</h2> */}
         <p className="text">{text ? text : "Sample Text"}</p>
     </div>
   )
---
+++ client/src/components/NewCard.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+import './components.css'
+
+const NewCard = () => {
+  return (
+    <div>NewCard</div>
+  )
+}
+
+const NewCardBubble = () => {
+  return (
+    <div className="bubble">
+      Add New Card
+    </div>
+  )
+}
+
+export default NewCard
