20899e13df436e4c30087201133da6fe84c2e7f4
sonOfArent <91688309+sonOfArent@users.noreply.github.com>
2022-12-07 12:26:44 -0600

    laptop

---
+++ server/data.js
@@ -0,0 +1,16 @@
+export const sets = {
+  language: [
+    ["help", "ayuda"],
+    ["two", "dos"],
+    ["too", "demasiado"],
+    ["big", "grande"],
+    ["about", "sobre"]
+  ],
+  science: [
+    ["positive", "proton"],
+    ["neutral", "neutron"],
+    ["negative", "electron"],
+    ["Fe", "iron"],
+    ["He", "helium"],
+  ]
+}
